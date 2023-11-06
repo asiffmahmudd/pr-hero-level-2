e6fa30523f5b4963d8a9bdc56deb2fc7c27ae59c
Asif Mahmud <asifmahmud3472@gmail.com>
2023-11-06 13:56:55 -0500

    module 2 practice

---
+++ typescript/module2/2.11.ts
@@ -0,0 +1,49 @@
+{
+    //utility type
+    type Person = {
+        name: string;
+        age: number;
+        email?: string;
+        contactNo: string;
+    }
+
+    //pick
+    type NameAge = Pick<Person, "name" | "age">
+    //omit
+    type ContactInfo = Omit<Person, "name" | "age">
+    //required
+    type PersonRequired = Required<Person>
+    //Partial
+    type PersonPartial = Partial<Person>
+
+    //readonly
+    type PersonReadOnly = Readonly<Person> 
+    const person1: PersonReadOnly = {
+        name: "Sarada",
+        age: 123,
+        contactNo: "456345"
+    } 
+
+    // person1.name = "Boruto"
+
+    //Record
+    // type MyObj = {
+    //     a: string;
+    //     b: string;
+    // }
+
+    type MyObj = Record<string, string>
+
+    const EmptyObj : Record<string, unknown> = {} 
+
+    const obj1 : MyObj = {
+        a: "aa",
+        b: "bb",
+        c: "cc",
+        d: "dd",
+        e: 6
+    }
+
+
+
+}
